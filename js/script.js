6fce2f63173cfd31d41388e92fb3246ef8e11100
anoopcse1 <156039060+anoopcse1@users.noreply.github.com>
2024-07-27 12:30:23 +1200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,76 +1,73 @@
 (function($) {
+  "use strict";
 
-    "use strict";
+  // init Chocolat light box
+  var initChocolat = function() {
+    Chocolat(document.querySelectorAll('.image-link'), {
+      imageSize: 'contain',
+      loop: true,
+    });
+  };
 
-        // init Chocolat light box
-        var initChocolat = function () {
-          Chocolat(document.querySelectorAll('.image-link'), {
-            imageSize: 'contain',
-            loop: true,
-          })
-        }
+  $(document).ready(function() {
+    initChocolat();
 
-    $(document).ready(function() {
+    /* Video */
+    var $videoSrc;  
+    $('.play-btn').click(function() {
+      $videoSrc = $(this).data("src");
+    });
 
-      initChocolat();
+    $('#myModal').on('shown.bs.modal', function (e) {
+      $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"); 
+    });
 
-      /* Video */
-      var $videoSrc;  
-        $('.play-btn').click(function() {
-          $videoSrc = $(this).data( "src" );
-        });
+    $('#myModal').on('hide.bs.modal', function (e) {
+      $("#video").attr('src', $videoSrc); 
+    });
+      
+    var swiper1 = new Swiper(".slider", {
+      effect: "fade",
+      pagination: {
+        el: ".slider-pagination",
+        clickable: true,
+      },
+    });
 
-        $('#myModal').on('shown.bs.modal', function (e) {
+    var swiper2 = new Swiper(".testimonial-swiper", {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        el: ".testimonial-pagination",
+        clickable: true,
+      },
+    });
 
-        $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
-      })
-
-      $('#myModal').on('hide.bs.modal', function (e) {
-        $("#video").attr('src',$videoSrc); 
-      })
-        
-      var swiper = new Swiper(".slider", {
-        effect: "fade",
-        pagination: {
-          el: ".slider-pagination",
-          clickable: true,
+    var swiper3 = new Swiper(".services-swiper", {
+      slidesPerView: 5,
+      spaceBetween: 120,
+      freeMode: true,
+      pagination: {
+        el: ".services-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
         },
-      });
-
-      var swiper = new Swiper(".testimonial-swiper", {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        pagination: {
-          el: ".testimonial-pagination",
-          clickable: true,
+        572: {
+          slidesPerView: 3,
         },
-      });
-
-      var swiper = new Swiper(".services-swiper", {
-        slidesPerView: 5,
-        spaceBetween: 120,
-        freeMode: true,
-        pagination: {
-          el: ".services-pagination",
-          clickable: true,
+        1024: {
+          slidesPerView: 4,
         },
-        breakpoints: {
-          0: {
-            slidesPerView: 1,
-          },
-          572: {
-            slidesPerView: 3,
-          },
-          1024: {
-            slidesPerView: 4,
-          },
-          1280: {
-            slidesPerView: 5,
-          },
+        1280: {
+          slidesPerView: 5,
         },
-      });
+      },
+    });
 
-          // product single page
+    // product single page
     var thumb_slider = new Swiper(".product-thumbnail-slider", {
       autoplay: true,
       loop: true,
@@ -82,7 +79,7 @@
 
     var large_slider = new Swiper(".product-large-slider", {
       autoplay: true,
-      loop:true,
+      loop: true,
       spaceBetween: 10,
       effect: 'fade',
       thumbs: {
@@ -90,48 +87,39 @@
       },
     });
 
-
-      window.addEventListener("load", (event) => {
-        //isotope
-        $('.isotope-container').isotope({
-          // options
-          itemSelector: '.item',
-          layoutMode: 'masonry',
-        });
-  
-  
-  
-        // Initialize Isotope
-        var $container = $('.isotope-container').isotope({
-          // options
-          itemSelector: '.item',
-          layoutMode: 'masonry',
-        });
-  
-        $(document).ready(function () {
-          //active button
-          $('.filter-button').click(function () {
-            $('.filter-button').removeClass('active');
-            $(this).addClass('active');
-          });
-        });
-  
-        // Filter items on button click
-        $('.filter-button').click(function () {
-          var filterValue = $(this).attr('data-filter');
-          if (filterValue === '*') {
-            // Show all items
-            $container.isotope({ filter: '*' });
-          } else {
-            // Show filtered items
-            $container.isotope({ filter: filterValue });
-          }
-        });
-  
+    window.addEventListener("load", function(event) {
+      // isotope
+      $('.isotope-container').isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'masonry',
       });
 
+      // Initialize Isotope
+      var $container = $('.isotope-container').isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'masonry',
+      });
 
+      // active button
+      $('.filter-button').click(function() {
+        $('.filter-button').removeClass('active');
+        $(this).addClass('active');
+      });
 
-    }); // End of a document
+      // Filter items on button click
+      $('.filter-button').click(function() {
+        var filterValue = $(this).attr('data-filter');
+        if (filterValue === '*') {
+          // Show all items
+          $container.isotope({ filter: '*' });
+        } else {
+          // Show filtered items
+          $container.isotope({ filter: filterValue });
+        }
+      });
+    });
+  }); // End of document ready function
 
 })(jQuery);
